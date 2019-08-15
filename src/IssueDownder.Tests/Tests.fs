dda679ca0bd1df2e4f550b86e89a8ff73b5a98d6
wk-j <somnuk.wk@gmail.com>
2019-08-15 20:29:46 +0700

    Download file

---
+++ src/IssueDownder.Tests/Tests.fs
@@ -0,0 +1,27 @@
+module Tests
+
+open System
+open Xunit
+open System.Text.RegularExpressions
+
+[<Fact>]
+let ``My test`` () =
+
+    let code =
+        """
+        [data-migrate.txt](https://github.com/bcircle/lion-migration/files/3496298/data-migrate.txt)
+        [data-migrate.xlsx](https://github.com/bcircle/lion-migration/files/3504556/data-migrate.xlsx)
+
+        Model alfresco
+        [ISO.zip](https://github.com/bcircle/lion-migration/files/3496319/ISO.zip)
+
+        Consult : Gif
+
+        ![code](https://user-images.githubusercontent.com/860704/63093104-1a5bdd00-bf8e-11e9-82ae-fa82bb5b0280.png)
+        """
+
+    let pattern = "\((?<link>[\S]+)\)"
+    let reg = Regex(pattern)
+    let matchs = reg.Matches(code) |> Seq.map(fun x -> x.Groups.["link"].Value)
+
+    ()
--- src/IssueDownder/Program.fs
+++ src/IssueDownder/Program.fs
@@ -1,12 +1,37 @@
 ﻿open System
 open LibGit2Sharp
 open Octokit
+open System.IO
+open System.Text.RegularExpressions
 
 let split k s = (s:String).Split (k:String)
 let replace k s = (s:String).Replace(k, String.Empty)
 
 let getRemotes (repo: LibGit2Sharp.Repository) =
     repo.Network.Remotes |> Seq.map(fun x -> x.Url)
+
+let download token (urls: string array)  =
+
+    for item in urls do
+        use client = new System.Net.Http.HttpClient()
+        client.DefaultRequestHeaders.Authorization <- System.Net.Http.Headers.AuthenticationHeaderValue("Token", token)
+        let data =
+            client.GetAsync(item)
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        let stream =
+            data.Content.ReadAsStreamAsync()
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+        let uri = Uri(item);
+        let fileName = System.IO.Path.GetFileName(uri.AbsolutePath);
+
+        let mode = FileMode.OpenOrCreate
+        let access = FileAccess.Write
+
+        use fileStream = new FileStream(fileName,  mode, access)
+        stream.CopyTo(fileStream)
 
 let getUser (repo: LibGit2Sharp.Repository) =
     let remote = getRemotes repo |> Seq.head
@@ -21,8 +46,13 @@ let getUser (repo: LibGit2Sharp.Repository) =
     | [user; repo] -> Some (user, repo)
     | _ -> None
 
-let getIssue user repo id =
-    let token = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN")
+let extractUrls code =
+    let pattern = "\((?<link>[\S]+)\)"
+    let reg = Regex(pattern)
+    let matchs = reg.Matches(code) |> Seq.map(fun x -> x.Groups.["link"].Value)
+    matchs
+
+let getIssue token user repo id =
     let client = GitHubClient(ProductHeaderValue("my-cool-app"));
     let tokenAuth = Credentials(token)
     let issue =
@@ -30,17 +60,19 @@ let getIssue user repo id =
         |> Async.AwaitTask
         |> Async.RunSynchronously
 
-    printfn "%A" (issue.Body)
+    issue.Body
 
 [<EntryPoint>]
 let main argv =
     let repo = new LibGit2Sharp.Repository(".")
     let data = getUser repo
+    let token = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN")
 
     match data with
     | Some (user, repo) ->
-        printfn "%A %A" user repo
-        getIssue user repo 1
+        let issueBody = getIssue token user repo 1
+        let links = extractUrls issueBody |> Seq.toArray
+        download token links
     | _ ->
         printfn "Invalid remote"
 
