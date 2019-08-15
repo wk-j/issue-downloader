ee0d0af84859b1c39da636c61149d0651c79bf9b
wk-j <somnuk.wk@gmail.com>
2019-08-15 18:43:32 +0700

    Get remote info

--- src/IssueDownder/Program.fs
+++ src/IssueDownder/Program.fs
@@ -1,8 +1,36 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open LibGit2Sharp
+
+let split k s = (s:String).Split (k:String)
+let replace k s = (s:String).Replace(k, String.Empty)
+
+let getRemotes (repo: Repository) =
+    repo.Network.Remotes |> Seq.map(fun x -> x.Url)
+
+let getUser (repo: Repository) =
+    let remote = getRemotes repo |> Seq.head
+    let tokens =
+        remote
+        |> replace "https://github.com/"
+        |> replace ".git"
+        |> split "/"
+        |> Seq.toList
+
+    match tokens with
+    | [user; repo] -> Some (user, repo)
+    | _ -> None
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    let repo = new LibGit2Sharp.Repository(".")
+    let data = getUser repo
+    match data with
+    | Some (user, repo) ->
+        printfn "%A %A" user repo
+    | _ ->
+        printfn "Invalid remote"
+
     0 // return an integer exit code
