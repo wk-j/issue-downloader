20df876741702dced40448c57626e33a17873648
wk-j <somnuk.wk@gmail.com>
2019-08-15 18:51:56 +0700

    Get issue body

--- src/IssueDownder/Program.fs
+++ src/IssueDownder/Program.fs
@@ -1,15 +1,14 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open LibGit2Sharp
+open Octokit
 
 let split k s = (s:String).Split (k:String)
 let replace k s = (s:String).Replace(k, String.Empty)
 
-let getRemotes (repo: Repository) =
+let getRemotes (repo: LibGit2Sharp.Repository) =
     repo.Network.Remotes |> Seq.map(fun x -> x.Url)
 
-let getUser (repo: Repository) =
+let getUser (repo: LibGit2Sharp.Repository) =
     let remote = getRemotes repo |> Seq.head
     let tokens =
         remote
@@ -22,15 +21,27 @@ let getUser (repo: Repository) =
     | [user; repo] -> Some (user, repo)
     | _ -> None
 
+let getIssue user repo id =
+    let token = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN")
+    let client = GitHubClient(ProductHeaderValue("my-cool-app"));
+    let tokenAuth = Credentials(token)
+    let issue =
+        client.Issue.Get(user, repo, id)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    printfn "%A" (issue.Body)
+
 [<EntryPoint>]
 let main argv =
-
     let repo = new LibGit2Sharp.Repository(".")
     let data = getUser repo
+
     match data with
     | Some (user, repo) ->
         printfn "%A %A" user repo
+        getIssue user repo 1
     | _ ->
         printfn "Invalid remote"
 
-    0 // return an integer exit code
+    0
